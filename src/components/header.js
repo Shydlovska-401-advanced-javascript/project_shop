e83c2a0beebbb43a6170ef57e0c6651fbb21daea
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-10-01 14:08:26 -0700

    added footer and header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { AppBar } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
+// import { IconButton  } from '@material-ui/core';
+// import { Button } from '@material-ui/core';
+import { Typography  } from '@material-ui/core';
 
 
 
 const Header = () => {
     return (
-        <div>
-           <h1>Header</h1> 
-        </div>
+        <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" >
+            <h2>Project Shop</h2>
+          </Typography>
+         
+        </Toolbar>
+      </AppBar>
     );
 }
 
