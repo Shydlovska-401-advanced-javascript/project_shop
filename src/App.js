46abc693f7df4a89b481c5b69f26646df1d935d8
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-10-01 12:18:13 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Header from './components/header.js'
+import Footer from './components/footer.js'
+// import Categories from './components/categories.js'
+// import Products from './components/products.js'
+// import Cart from './components/cart.js'
 
 
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
-    </div>
+   <>
+   <Header />
+   {/* <Categories />
+   <Products />
+   <Cart /> */}
+   <Footer />
+   </>
+  
   );
 }
 
