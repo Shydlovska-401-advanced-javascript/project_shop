ac4c891e39190e28316e644f672a9f7707a2abac
Alexander Whan <whan.alexander@gmail.com>
2020-10-01 15:42:43 -0700

    Added useEffect to render products array on page load

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   footer: {
     backgroundColor: '#eee',
     padding: theme.spacing(6),
@@ -10,16 +10,15 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Footer = props => {
-
   const classes = useStyles();
 
   return (
     <footer className={classes.footer}>
-    <Typography variant="h7" align="center" gutterBottom>
-      &copy; JS Code Fellows
+      <Typography variant="h6" align="center" gutterBottom>
+        &copy; JS Code Fellows
       </Typography>
-  </footer>
+    </footer>
   );
-}
+};
 
 export default Footer;
--- src/store/products.js
+++ src/store/products.js
@@ -43,6 +43,9 @@ export default (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
+    case 'GET_PRODUCTS':
+      return { ...state, products: payload };
+
     default:
       return state;
   }
